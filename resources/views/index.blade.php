f34629e3ca19772b9829a0a53608b9d3b71cabd5
Ubuntu <ubuntu@ip-172-31-37-164.ap-northeast-1.compute.internal>
2020-10-29 06:44:18 +0000

    初めてのcommit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ログイン画面</title>
+</head>
+<body>
+  
+  <h1>TIME SCHEDULE  MANAGEMENT</h1>
+
+<a href="">ログインする</a>
+<a href="">新規アカウント登録</a>
+
+</body>
+</html>
